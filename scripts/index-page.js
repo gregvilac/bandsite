b0bfb12a3a1008cdfb3e5259e4b2f0ec950219df
Greg Vilac <gregvilac@hotmail.com>
2022-10-20 17:31:09 -0700

    start bio javascript

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -16,11 +16,11 @@ const comments = [
   },
 ];
 
-var today = new Date();
-var date =
-  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-var time =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date + " " + time;
+// var today = new Date();
+// var date =
+//   today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+// var time =
+//   today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var dateTime = date + " " + time;
 
-console.log(dateTime);
+// console.log(dateTime);
